695b8e06b8ca1207b1371cee2875a0d313626fd9
Christopher Monterroso <chrismonc08@gmail.com>
2025-03-04 22:30:10 -0600

    api cs y sql server

---
+++ api-cs/Prueba/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Prueba.Data;
+using Prueba.Models;
+
+namespace Prueba.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProductos()
+        {
+            var productos = _context.Productos.ToList();
+            return Ok(productos);
+        }
+
+        [HttpPost]
+        public IActionResult AgregarProducto([FromBody] Producto producto)
+        {
+            _context.Productos.Add(producto);
+            _context.SaveChanges();
+            return Ok(producto);
+        }
+    }
+}
---
+++ api-cs/Prueba/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba.Models;
+
+namespace Prueba.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
